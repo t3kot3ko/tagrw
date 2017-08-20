d56b528e4ba0bd3ecdf78d818f64b8315f588369
Tomoyuki KAMIYA <kamiya7140@gmail.com>
2017-08-20 23:31:15 +0900

    add cli endpoint to invoke commands

--- lib/tagrw.rb
+++ lib/tagrw.rb
@@ -2,46 +2,110 @@ require "tagrw/version"
 require "mp3info"
 
 module Tagrw
-	# Write v1/v2 tags for artist/album/title/track#
-	def self.write_tag(filepath, artist, album, title, track)
-		Mp3Info.open(filepath) do |mp3|
-			mp3.tag.artist = artist
-			mp3.tag2.TPE1 = artist
+  # TODO: separate writing buffer from Data
+  # pictures = array of paths to pictures
+  class Data < Struct.new(:filepath, :track, :artist, :album, :title, :pictures)
+    EMPTY = "(empty)"
 
-			mp3.tag.album = album
-			mp3.tag2.TALB = album
+    def initialize(*args)
+      super(*args)
+      self.pictures ||= []
+    end
 
-			mp3.tag2.TRCK = track
-		end
-	end
+    def to_s
+      empty = "(empty)"
+      return pretty_format(
+        self.filepath || EMPTY, 
+        self.track || EMPTY,
+        self.artist || EMPTY, 
+        self.album || EMPTY, 
+        self.title || EMPTY, 
+      )
+    end
 
-	def self.show(filepath)
-		Mp3Info.open(filepath) do |mp3|
-			# artist
-			tag_artist = mp3.tag.artist
-			tag2_tpe1 = mp3.tag2.TPE1
+    def diff(other)
+      data = [:filepath, :track, :artist, :album, :title].map do |key|
+        if self.send(key) == other.send(key)
+          "#{self.send(key) || EMPTY} (same)"
+        else
+          "#{self.send(key) || EMPTY} => #{other.send(key) || EMPTY}"
+        end
+      end
+      pretty_format(*data)
+    end
 
-			# album
-			tag_album = mp3.tag.album
-			tag2_talb = mp3.tag2.TALB
+    private 
+    def pretty_format(filepath, track, artist, album, title)
+      return <<~EOF
+      * #{File.basename(filepath)}
+        |-- track:  #{track || empty}
+        |-- artist: #{artist|| empty}
+        |-- album:  #{album || empty}
+        |-- title:  #{title || empty}
+      EOF
 
-			# track
-			track = mp3.tag2.TRCK
+    end
+  end
 
-			puts [track, mp3.tag.title, tag_artist, tag_album].join("\t")
-		end
-	end
+  # Write v1/v2 tags for artist/album/title/track#
+  def self.write_tag(data)
+    Mp3Info.open(data.filepath) do |mp3|
+      mp3.tag.artist = data.artist
+      mp3.tag2.TPE1 =  data.artist
 
-	# TODO: use regexp
-	def self.valid_filename?(filename, delimiter)
-		ext = File.extname(filename)
-		return false if ext.downcase != ".mp3"
+      mp3.tag.album = data.album
+      mp3.tag2.TALB = data.album
 
-		basename = File.basename(filename, ext)
-		index, *title = basename.split(delimiter)
+      mp3.tag2.TRCK = data.track
+      mp3.tag.title = data.title
 
-		return false if title.empty?
+      # picture
+      unless data.pictures.empty?
+        mp3.tag2.remove_pictures    # remove all images
+        data.pictures.each do |path|
+          next unless File.exists?(path)
+          next unless %w(.jpg .png).include?(File.extname(path).downcase)
 
-		return true
-	end
+          mp3.tag2.add_picture(File.open(path, "rb").read)
+        end
+      end
+    end
+  end
+
+  def self.inspect(filepath)
+    Mp3Info.open(filepath) do |mp3|
+      # TODO: the case when v1/v2 tags are different
+      
+      # artist
+      tag_artist = mp3.tag.artist
+      tag2_tpe1 = mp3.tag2.TPE1
+
+      # album
+      tag_album = mp3.tag.album
+      tag2_talb = mp3.tag2.TALB
+
+      # track
+      track = mp3.tag2.TRCK
+
+      title = mp3.tag.title
+
+      return Data.new(filepath, track, tag_artist, tag_album, title)
+    end
+  end
+
+  def self.print(filepath, track, artist, album, title)
+  end
+
+  # TODO: use regexp
+  def self.valid_filename?(filename, delimiter)
+    ext = File.extname(filename)
+    return false if ext.downcase != ".mp3"
+
+    basename = File.basename(filename, ext)
+    index, *title = basename.split(delimiter)
+
+    return false if title.empty?
+
+    return true
+  end
 end
